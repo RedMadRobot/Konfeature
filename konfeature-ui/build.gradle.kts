55d8601af15d0862ee903f1245d098537b5fc44a
Yuriy Elistratov <i.elistratov@redmadrobot.com>
2026-04-14 04:30:41 +0400

    Create konfeature ui module

---
+++ konfeature-ui/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("multiplatform")
+    id("com.android.kotlin.multiplatform.library")
+    alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
+    convention.detekt
+}
+
+description = "Compose Multiplatform UI for managing Konfeature"
+
+kotlin {
+    explicitApi()
+    jvmToolchain(17)
+
+    android {
+        namespace = "com.redmadrobot.konfeature.ui"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(projects.konfeature)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+        }
+    }
+}
